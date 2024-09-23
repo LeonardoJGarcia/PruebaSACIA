1476affb3f19603458f3ab7f0a5d67ecc1891cdd
LeonardoJGarcia <126028905+LeonardoJGarcia@users.noreply.github.com>
2024-09-23 11:28:21 -0600

    Update ChatGptWidget.js turbo

--- ChatGptWidget.js
+++ ChatGptWidget.js
@@ -93,7 +93,7 @@
             "Authorization": "Bearer " + apiKey
           },
           body: JSON.stringify({
-            "model": "gpt-3.5-turbo-0125",
+            "model": "gpt-3.5-turbo",
             "prompt": prompt,
             "max_tokens": parseInt(max_tokens),
             "n": 1,
